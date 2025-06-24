3f1d2c820b11721704514fe4bc21dcf11ac8d3b1
PavelKozaev <pkozaev221299@gmail.com>
2025-06-24 23:56:43 +0300

    Add logging

---
+++ NewsParser/Services/FileLogger.cs
@@ -0,0 +1,14 @@
+﻿using NewsParser.Interfaces;
+
+namespace NewsParser.Services
+{
+    public class FileLogger(string filePath) : ILogger
+    {
+        private readonly string _filePath = filePath;
+
+        public void Log(string message)
+        {
+            File.AppendAllText(_filePath, $"{DateTime.Now}: {message}{Environment.NewLine}");
+        }
+    }
+}
