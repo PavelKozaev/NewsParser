09a04dc0dcfda15ec4ccb113f35f91dc777c5219
PavelKozaev <pkozaev221299@gmail.com>
2025-06-24 21:46:08 +0300

    Add model

---
+++ NewsParser/Models/NewsItem.cs
@@ -0,0 +1,9 @@
+﻿namespace NewsParser.Models
+{
+    public class NewsItem
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Date { get; set; }
+    }
+}
