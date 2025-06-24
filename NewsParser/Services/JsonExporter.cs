b17796f0a7601806b22a0481e3c93f2c450ae075
PavelKozaev <pkozaev221299@gmail.com>
2025-06-25 00:11:34 +0300

    Implement a service for exporting data to a file

---
+++ NewsParser/Services/JsonExporter.cs
@@ -0,0 +1,20 @@
+﻿using NewsParser.Models;
+using System.Text.Json;
+
+namespace NewsParser.Services
+{
+    public class JsonExporter
+    {
+        public void ExportToFile(List<NewsItem> newsItems, string filePath)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var json = JsonSerializer.Serialize(newsItems, options);
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
