678927adc316d90736b23a79321962eb78fd9f37
PavelKozaev <pkozaev221299@gmail.com>
2025-06-25 00:09:30 +0300

    Add a news filtering service

---
+++ NewsParser/Services/NewsFilter.cs
@@ -0,0 +1,37 @@
+﻿using NewsParser.Interfaces;
+using NewsParser.Models;
+
+namespace NewsParser.Services
+{
+    public class NewsFilter
+    {
+        public List<NewsItem> FilterByLast30Days(List<NewsItem> newsItems, ILogger logger, out int filteredCount)
+        {
+            filteredCount = 0;
+            var result = new List<NewsItem>();
+            var cutoffDate = DateTime.Now.AddDays(-30);
+
+            foreach (var item in newsItems)
+            {
+                if (DateTime.TryParse(item.Date, out var newsDate))
+                {
+                    if (newsDate >= cutoffDate)
+                    {
+                        result.Add(item);
+                    }
+                    else
+                    {
+                        filteredCount++;
+                    }
+                }
+                else
+                {
+                    logger.Log($"Invalid date format in filtered item: {item.Date}");
+                    filteredCount++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
